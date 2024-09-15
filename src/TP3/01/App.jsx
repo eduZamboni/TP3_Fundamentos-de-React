88891a3102a502c40137b6813b9224b62118fa13
eduZamboni <128424472+eduZamboni@users.noreply.github.com>
2024-09-15 16:13:34 -0300

    TP3

---
+++ src/TP3/01/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from './Components/Greeting';
+
+export default function App() {
+  return (
+    <div>
+      <Greeting name="João" />
+      <Greeting name="Maria" />
+      <Greeting name="Pedro" />
+    </div>
+  );
+}
---
+++ src/TP3/01/Components/Greeting.jsx
@@ -0,0 +1,5 @@
+
+
+export default function Greeting(props) {
+    return <h1>Olá, {props.name}!</h1>;
+}
